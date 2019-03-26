3a726ef33e40b48caecdcd563bd723af8f7cbfef
Emily DeWitt <emilykdewitt@gmail.com>
2019-03-25 20:11:35 -0500

    Added first functions

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('Hello World!');
+const addButton = document.getElementById('addButton');
+const inputIngredient = document.getElementById('inputIngredient');
+
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+};
+
+const addIngredient = () => {
+    const inputText = inputIngredient.nodeValue;
+    console.log(inputText);
+}
+
+const eventListeners = () => {
+    addButton.addEventListener('click', addIngredient);
+};
+
+const init = () => {
+    eventListeners();
+};
+
+init();
